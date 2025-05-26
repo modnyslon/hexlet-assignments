84715627f108409c18ca638ecfc27202c8ebfb22
@hexlet/cli <support@hexlet.io>
2025-05-26 18:43:39 +0300

    submit java-spring-boot-ru/jpa-comparing-entities

---
+++ java-spring-boot-ru/jpa-comparing-entities/src/main/java/exercise/model/Product.java
@@ -0,0 +1,31 @@
+package exercise.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+// BEGIN
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Product {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @EqualsAndHashCode.Include
+    private String title;
+
+    @EqualsAndHashCode.Include
+    private int price;
+}
+// END
